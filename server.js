2682180c7d4695ab2d393f1cdc664ba4c50747a2
Tmaneesh19 <maneeshkumar19022006@gmail.com>
2024-10-05 16:19:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const exp = require('express')
+const app = exp();
+// this internally creates http server
+
+let usersList = [
+    {
+    id:1,
+    name:'maneesh'
+    },
+    {
+    id:2,
+    name:'kumar'
+    }
+]
+
+app.get('/users',(req,res)=>{
+    res.send({abc:usersList})
+})
+
+app.get('/users/:id',(req,res)=>{
+    let id = Number(req.params.id);
+    let a = usersList.find((c)=>c.id===id);
+    res.send({payload:a});
+});
+// attach port number.
+app.listen(4000,()=>console.log('server started at 4000 port number'))
