f6d522741f25a87297ce694933552e97abfb22f2
Tmaneesh19 <130675805+Tmaneesh19@users.noreply.github.com>
2024-10-12 18:45:49 +0530

    d

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const exp = require('express')
 const app = exp();
-//  internally ( establishe s ) the http server............askkndkasndkn... .
+//  internal ly ( establishe s ) the http server............askkndkasndkn... .
 //sccsd jn aaasd af aaaa a a 
 
 app.use(exp.json());
